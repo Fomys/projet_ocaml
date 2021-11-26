bf66e3a6563df4578711a593bf7d89f3a35b24fb
Paille Celestine <paille@insa-10223>
2021-11-26 11:45:54 +0100

    update_flot

--- lib/algorithms.ml
+++ lib/algorithms.ml
@@ -1,6 +1,6 @@
 open Tools
 open Graph
-open Printf
+open List
 
 (* let ford_fulkerson_rec gr_cap gr_flot source sink = ()
 
@@ -46,23 +46,31 @@ let graph_gap gr_cap gr_flot =
         )
         (clone_nodes gr_cap)
 
-
 let rec get_min_on_path_rec graph path poids_min =
     match path with
         | [] -> poids_min
-        | [(source, destination)::rest] ->
+        | (source, destination)::rest ->
             match find_arc graph source destination with
                 | None -> 0
-                | Some w -> get_min_on_path_rec graph path (if w < poids_min then w else poids_min)
+                | Some w -> get_min_on_path_rec graph rest (if w < poids_min then w else poids_min)
 
 let get_min_on_path graph path =
     get_min_on_path_rec graph path 0
 
+let rec update_flot gr_flot path min = 
+    match path with 
+        | [] -> gr_flot 
+        | (node1,node2)::rest -> match find_arc gr_flot node1 node2 with 
+                                     | None -> update_flot ( new_arc gr_flot node1 node2 min ) rest min
+                                     | Some w -> update_flot ( new_arc gr_flot node1 node2 ( w + min ) ) rest min
+                                 
+
+
 
 let ford_fulkerson_rec gr_cap gr_flot source sink =
-    match find_path_source_to_sink source sink with
+    match find_path_source_to_sink (graph_gap gr_cap gr_flot) source sink with
         | [] -> gr_flot
-        | path -> update_flot path (get_min_on_path gr_flot path )
+        | path -> update_flot gr_flot path (get_min_on_path gr_flot path )
 
 let ford_fulkerson gr source sink =
     ford_fulkerson_rec gr (clone_nodes gr) source sink
