a048aca5ca5769333e72fc2611fe9ad3284179f2
Paille Celestine <paille@insa-11277>
2021-12-03 10:12:39 +0100

    detail

--- bin/main.ml
+++ bin/main.ml
@@ -7,9 +7,7 @@ let () =
   let graph_capacite_int = gmap graph_capacite (fun v -> int_of_string v) in
   let graph_flot = from_file "flot.gr" in
   let graph_flot_int = gmap graph_flot (fun v -> int_of_string v) in
-
-  (*  let graph_out_int = graph_gap graph_capacite_int graph_flot_int in*)
-
+  let graph_out_int = graph_gap graph_capacite_int graph_flot_int in
 
   (* Rewrite the graph that has been read. *)
   let graph_capacite_str = gmap graph_capacite_int (fun v -> string_of_int v) in
--- lib/algorithms.mli
+++ lib/algorithms.mli
@@ -1,3 +1,4 @@
 open Graph
 
+val graph_gap: int graph -> int graph -> int graph
 val ford_fulkerson: int graph -> id -> id -> int graph
