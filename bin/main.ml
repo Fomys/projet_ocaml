82a480b953db063bd41d1f72f1619182b8e0217a
Louis Chauvet <louis.chauvet@free.fr>
2022-01-05 13:32:07 +0100

    step

--- bin/main.ml
+++ bin/main.ml
@@ -4,14 +4,14 @@ open Projet.Algorithms
 
 let () =
   let graph_capacite = from_file "graph2.gr" in
-  let graph_capacite_int = gmap graph_capacite (fun v -> int_of_string v) in
+  let graph_capacite_int = gmap graph_capacite (fun v -> float_of_string v) in
 
   let graph_flot_fulkerson = ford_fulkerson graph_capacite_int 0 12 in
 
 
   (* Rewrite the graph that has been read. *)
-  let graph_capacite_str = gmap graph_capacite_int (fun v -> string_of_int v) in
-  let graph_flot_fulkerson_str = gmap graph_flot_fulkerson (fun v -> string_of_int v) in
+  let graph_capacite_str = gmap graph_capacite_int (fun v -> string_of_float v) in
+  let graph_flot_fulkerson_str = gmap graph_flot_fulkerson (fun v -> string_of_float v) in
   let () = write_file "graph_capacite_str.txt" graph_capacite_str in
   let () = write_file "graph_flot_fulkerson_str.txt" graph_flot_fulkerson_str in
 
--- lib/tricount.ml
+++ lib/tricount.ml
@@ -1,15 +1,24 @@
+open Graph
+open Tools
+
 let rec get_id_for_name names name =
     match names with
-        | [] -> ([name], 0)
+        | [] -> ([name], 2)
         | head :: rest ->
             if head = name then
-                (head :: rest, 0)
+                (head :: rest, 2)
             else
                 let (new_names, id) = get_id_for_name rest name in
                 (head :: new_names, id + 1)
 
 let read_file path =
     let infile = open_in path in
+    let add_indebted shared_amount (graph, names) name =
+        let (names, indebted_id) = get_id_for_name names name in
+        let graph = new_node graph indebted_id in
+        let graph = add_arc graph 0 indebted_id shared_amount in
+        (graph, names)
+    in
     let rec loop graph names =
         try
             let line = input_line infile in
@@ -22,11 +31,11 @@ let read_file path =
                         let feeder = List.nth line 0 in
                         let indebted = List.tl (List.tl line) in
                         let amount = float_of_string (List.nth line 1) in
+                        let shared_amount = amount /. Float.of_int (List.length indebted) in
                         let (names, feeder_id) = get_id_for_name names feeder in
-                        let feeder_id = feeder_id + 2 in
-                            (* + 2 car il y a deja les noeuds 0 et 1 dans le graphe "fausse source" "fausse destination" *)
-                        let graph = try_add_node graph feeder_id in
+                        let graph = new_node graph feeder_id in
                         let graph = add_arc graph feeder_id 1 amount in
+                        let (graph, names) = List.fold_left (add_indebted shared_amount) (graph, names) indebted in
                         loop graph names
         with End_of_file -> (graph, names)
     in
@@ -34,7 +43,6 @@ let read_file path =
     close_in infile;
     (final_graph, names)
 
-let solve_tricount in_file out_file =
+let solve_tricount in_file _ =
+    let _ = read_file in_file in
     ()
-
-
