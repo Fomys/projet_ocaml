0cadd6b29cd1239fb356af9097c214ed5cf034ea
Louis Chauvet <louis.chauvet@free.fr>
2022-01-05 13:06:59 +0100

    step

--- lib/tricount.ml
+++ lib/tricount.ml
@@ -1,4 +1,4 @@
-let get_id_for_name names name =
+let rec get_id_for_name names name =
     match names with
         | [] -> ([name], 0)
         | head :: rest ->
@@ -16,53 +16,23 @@ let read_file path =
             let line = String.trim line in
             if line = "" then (graph, names)
             else
-                let line = String.split_on_char " " line in
+                let line = String.split_on_char ' ' line in
                     if List.length line < 3 then (graph, names)
                     else
                         let feeder = List.nth line 0 in
                         let indebted = List.tl (List.tl line) in
-                        let amount = int_of_string (List.nth line 1) in
+                        let amount = float_of_string (List.nth line 1) in
                         let (names, feeder_id) = get_id_for_name names feeder in
-                        let graph = try_add_node graph (feeder_id + 2) in
-                                                (* + 2 car il y a deja les noeuds 0 et 1 dans le graphe "fausse source" "fausse destination" *)
-
-
-
-
-
-
-
-      let infile = open_in path in
-
-      (* Read all lines until end of file. *)
-      let rec loop graph =
-        try
-          let line = input_line infile in
-
-          (* Remove leading and trailing spaces. *)
-          let line = String.trim line in
-
-          let graph2 =
-            (* Ignore empty lines *)
-            if line = "" then graph
-
-            (* The first character of a line determines its content : n or e. *)
-            else match line.[0] with
-              | 'n' -> read_node graph line
-              | 'e' -> read_arc graph line
-
-              (* It should be a comment, otherwise we complain. *)
-              | _ -> read_comment graph line
-          in
-          loop graph2
-
-        with End_of_file -> graph (* Done *)
-      in
-
-      let final_graph = loop empty_graph in
-
-      close_in infile ;
-      final_graph
+                        let feeder_id = feeder_id + 2 in
+                            (* + 2 car il y a deja les noeuds 0 et 1 dans le graphe "fausse source" "fausse destination" *)
+                        let graph = try_add_node graph feeder_id in
+                        let graph = add_arc graph feeder_id 1 amount in
+                        loop graph names
+        with End_of_file -> (graph, names)
+    in
+    let (final_graph, names) = loop empty_graph [] in
+    close_in infile;
+    (final_graph, names)
 
 let solve_tricount in_file out_file =
     ()
