c4d7b1861dc1f5bf7406c85efdf63cd5a34cc911
Paille Celestine <paille@localhost>
2022-01-05 13:31:44 +0100

    nimportkoi

--- lib/tricount.ml
+++ lib/tricount.ml
@@ -1,12 +1,19 @@
-let get_id_for_name names name =
+open Graph
+
+let try_add_node gr feeder_id =
+    if node_exists gr feeder_id then gr
+    else let graph = (feeder_id, []) :: gr in
+        n_fold graph ( fun gr node -> new_arc (new_arc gr feeder_id node Float.infinity) node feeder_id Float.infinity ) graph
+
+let rec get_id_for_name names name = (*recherche le tupple nom et id associé*)
     match names with
-        | [] -> ([name], 0)
+        | [] -> ([name], 2) (*si name n'apparaît pas dans names, je lui donne l'id 2 (0 pour source et 1 pour sink) et arrête la recherche*)
         | head :: rest ->
             if head = name then
-                (head :: rest, 0)
+                (head :: rest, 2) (*si name apparait en haut de names, je lui donne l'id 2 et arrête la recherche*)
             else
                 let (new_names, id) = get_id_for_name rest name in
-                (head :: new_names, id + 1)
+                (head :: new_names, id + 1) (*sinon, je continue la recherche sans oublier d'incrémenter l'id trouver*)
 
 let read_file path =
     let infile = open_in path in
